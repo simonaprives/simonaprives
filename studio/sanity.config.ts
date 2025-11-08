68aa9c7a001e85dd7484400d079e7565de61dab6
andypbrowne <andypbrowne@gmail.com>
2025-11-08 18:45:13 -0500

    feat(studio): enable custom desk structure

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -2,6 +2,7 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
+import deskStructure from './deskStructure'
 
 export default defineConfig({
   name: 'default',
@@ -10,7 +11,7 @@ export default defineConfig({
   projectId: 'xkgmhl6j',
   dataset: 'production',
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [structureTool({structure: deskStructure}), visionTool()],
 
   schema: {
     types: schemaTypes,
