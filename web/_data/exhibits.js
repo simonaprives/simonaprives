7f192f19907024ce3610814e04fc47d4dc8562ba
andypbrowne <andypbrowne@gmail.com>
2025-12-25 16:31:30 -0500

    add view transition api

---
+++ web/_data/exhibits.js
@@ -0,0 +1,33 @@
+const groq = require('groq')
+const client = require('../utils/sanityClient')
+const imageUrlBuilder = require('@sanity/image-url')
+
+const builder = imageUrlBuilder(client)
+
+module.exports = async function() {
+  const query = groq`*[_type == "artworkExhibits"] | order(orderRank) {
+    title,
+    "slug": slug.current,
+    description,
+    venue,
+    location,
+    photoCredit,
+    mainImage,
+    images
+  }`
+
+  const docs = await client.fetch(query).catch(err => { console.error(err); return [] })
+
+  // Add preview image URLs
+  return docs.map(doc => {
+    let previewImageUrl = null
+    if (doc.mainImage) {
+      try {
+        previewImageUrl = builder.image(doc.mainImage).width(800).url()
+      } catch (err) {
+        previewImageUrl = null
+      }
+    }
+    return Object.assign({}, doc, { previewImageUrl })
+  })
+}
